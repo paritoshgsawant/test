4b1fbf6d42081206a62dec4c0bac389bd60186fa
paritoshgsawant <pari11sawant@gmail.com>
2021-05-12 01:02:51 +0530

    first commit

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,10 +13,10 @@ export class CourseComponent implements OnInit {
   }
 
   course=[
-    {'id':1 , 'name':'learn angular','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'../../assets/images/angular.jpg'},
-    {'id':2 , 'name':'learn  JavaScript ','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'../../assets/images/js.jpg'},
-    {'id':3 , 'name':'learn PHP ','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'../../assets/images/php.jpg'},
-    {'id':4 , 'name':'learn HTML ','discription':'  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'../../assets/images/html.jpg'}
+    {'id':1 , 'name':'learn angular','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'./assets/images/angular.jpg'},
+    {'id':2 , 'name':'learn  JavaScript ','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'./assets/images/js.jpg'},
+    {'id':3 , 'name':'learn PHP ','discription':' scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'./assets/images/php.jpg'},
+    {'id':4 , 'name':'learn HTML ','discription':'  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.','image':'./assets/images/html.jpg'}
     
   ]
 
